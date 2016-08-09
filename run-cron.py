c68822c59036de2636b0a4712cd7e16642b8dbdb
Mingqi Shao <shaomq@gmail.com>
2016-08-09 22:14:04 +0800

    fix a bug: dont involve empty system environment variables

--- run-cron.py
+++ run-cron.py
@@ -4,6 +4,7 @@ import os
 from os import path
 from subprocess import call
 
+
 def empty_dir(d):
     for the_file in os.listdir(d):
         file_path = path.join(d, the_file)
@@ -11,17 +12,18 @@ def empty_dir(d):
             if path.isfile(file_path):
                 os.unlink(file_path)
         except Exception, e:
-            print e    
+            print e
 
 
 if __name__ == '__main__':
-    empty_dir('/etc/cron.d/') 
+    empty_dir('/etc/cron.d/')
     env_header = ''
     for env_n, env_v in os.environ.items():
-        env_header = env_header + env_n + '='+ env_v + '\n'
-    for cronfile in  os.listdir('/cron'):
+        if len(env_v) > 0:
+            env_header = env_header + env_n + '=' + env_v + '\n'
+    for cronfile in os.listdir('/cron'):
         with open(path.join('/cron', cronfile), 'r') as fread:
-            with open(path.join('/etc/cron.d/', cronfile.replace('.','_')), 'w') as fwrite:
+            with open(path.join('/etc/cron.d/', cronfile.replace('.', '_')), 'w') as fwrite:
                 fwrite.write(env_header)
                 fwrite.write('\n')
                 fwrite.write(fread.read())
